cdbe0c49df35b49d6f172e6b54cbb035743233dc
Tnaike <timmynaike@gmail.com>
2024-01-24 14:35:16 +0100

    Add section for users display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,16 +35,16 @@ export default function Home() {
           </div>
         </div>
       </NavBar>
-      <main className='relative scroll-smooth'>
-        <section className='pt-12 pb-12'>
-          <div className='md:container sm:container md:px-8 mx-auto'>
+      <main className='relative scroll-smooth bg-white'>
+        <section className='pt-12 pb-12 bg-[#f9f9f9]'>
+          <div className='container md:container px-4 md:px-8 mx-auto'>
             <div className='grid grid-cols-1 md:gap-8 md:grid-cols-2'>
               <div className='flex items-top row-start-2 md:row-start-1'>
                 <div>
-                  <h1 className='flex flex-col text-xxl font-bolder text-gray-00'>
+                  <h1 className='flex flex-col text-xl md:text-xxl mt-5 font-bolder text-gray-500'>
                     Streamline Issue Tracking with BugTracker
                   </h1>
-                  <div className='font-display text-xs md:text-base lg:text-md'>
+                  <div className='font-display text-sm md:text-base lg:text-md'>
                     <p className='my-4'>
                       A simple, fast and scalable bug tracking system that helps
                       you manage, track and resolve issues efficiently,
@@ -64,6 +64,14 @@ export default function Home() {
                 />
               </div>
             </div>
+          </div>
+        </section>
+        <section className='sm:py-6 md:py-8 text-gray-500'>
+          <div className='container mx-auto'>
+            <h2 className='font-display text-base font-medium xl:text-lg mb-4 text-center text-gray-400/80'>
+              Thousands of businesses scale faster and more efficiently by
+              building on BugTracker.
+            </h2>
           </div>
         </section>
       </main>
