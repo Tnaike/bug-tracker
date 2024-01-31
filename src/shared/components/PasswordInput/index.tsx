751a1963862058cc20ed1acf6366c91c9da9786a
Tnaike <timmynaike@gmail.com>
2024-01-31 02:18:02 +0100

    Show Password icon

--- src/shared/components/PasswordInput/index.tsx
+++ src/shared/components/PasswordInput/index.tsx
@@ -1,5 +1,6 @@
 import React, { InputHTMLAttributes, useState } from 'react';
 import TextInput from '@/shared/components/TextInput';
+import Image from 'next/image';
 
 interface PasswordInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -64,47 +65,25 @@ const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
 PasswordInput.displayName = 'PasswordInput';
 
 const ShowPasswordIcon: React.FC = () => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    fill='none'
-    viewBox='0 0 24 24'
-    strokeWidth={1.5}
-    stroke='currentColor'
-    className='h-5 w-5'
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      d='M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'
-    />
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
-    />
-  </svg>
+  <Image
+    className='w-full'
+    src='/images/showPassword-icon.svg'
+    alt='Password icon'
+    width={24}
+    height={24}
+    priority
+  />
 );
 
 const HidePasswordIcon: React.FC = () => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    fill='none'
-    viewBox='0 0 24 24'
-    strokeWidth={1.5}
-    stroke='currentColor'
-    className='h-5 w-5'
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      d='M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'
-    />
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
-    />
-  </svg>
+  <Image
+    className='w-full'
+    src='/images/hidePassword-icon.svg'
+    alt='Password icon'
+    width={24}
+    height={24}
+    priority
+  />
 );
 
 export default PasswordInput;
