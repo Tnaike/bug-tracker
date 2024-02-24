55c394334504829ee526b892e40b108007ab92cc
Tnaike <timmynaike@gmail.com>
2024-02-24 03:23:18 +0100

    Refactor validation schema

--- src/lib/validations/auth.schema.ts
+++ src/lib/validations/auth.schema.ts
@@ -1,27 +1,24 @@
 import { z } from 'zod';
 
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-+_!@#$%^&*.,?]).+$/;
+const commonPasswordRules = z
+  .string()
+  .min(1, 'Password is required')
+  .min(6, 'Password must be more than 6 characters')
+  .regex(passwordRegex, 'Password must contain a number, special character, both capital and small letter');
 
 export const SignUpUserSchema = z.object({
   firstName: z.string().min(1, 'First name is required').min(3, 'First name must be at least 3 characters'),
   lastName: z.string().min(1, 'Last name is required').min(3, 'Last name must be at least 3 characters'),
   email: z.string().min(1, 'Email address is required').email('Email address is invalid'),
-  password: z
-    .string()
-    .min(1, 'Password is required')
-    .min(6, 'Password must be more than 6 characters')
-    .regex(passwordRegex, 'Password must contain a number, special character, both capital and small letter'),
+  password: commonPasswordRules,
 });
 
 export type SignUpUserInput = z.infer<typeof SignUpUserSchema>;
 
 export const SignInUserSchema = z.object({
   email: z.string().min(1, 'Email address is required').email('Email address is invalid'),
-  password: z
-    .string()
-    .min(1, 'Password is required')
-    .min(6, 'Password must be more than 6 characters')
-    .regex(passwordRegex, 'Password must contain a number, special character, both capital and small letter'),
+  password: commonPasswordRules,
 });
 
 export type SignInUserInput = z.infer<typeof SignInUserSchema>;
