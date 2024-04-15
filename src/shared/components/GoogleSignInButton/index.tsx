0ece578db76037ce785614e914d8e3c56c8860e4
Tnaike <timmynaike@gmail.com>
2024-04-15 01:30:55 +0100

    Add loading to google auth button

--- src/shared/components/GoogleSignInButton/index.tsx
+++ src/shared/components/GoogleSignInButton/index.tsx
@@ -4,11 +4,21 @@ import ROUTE from '@/app/routes';
 import Button from '@/shared/components/Button';
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
+import { useState } from 'react';
 import googleIcon from '/public/images/color-google-icon.svg';
 
 const GoogleSignInButton = () => {
-  const loginWithGoogle = () => {
-    signIn('google', { callbackUrl: ROUTE.dashboard });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loginWithGoogle = async () => {
+    try {
+      setIsLoading(true);
+      await signIn('google', { callbackUrl: ROUTE.dashboard });
+    } catch (error) {
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -16,7 +26,8 @@ const GoogleSignInButton = () => {
       onClick={loginWithGoogle}
       label="Sign in with Google"
       size="medium"
-      className='!p-0 !pr-6 !rounded-full'
+      className={`!p-0 !pr-6 !rounded-full !min-h-[41px] ${isLoading ? '!pl-4' : ''}`}
+      isLoading={isLoading}
       svgIcon={
         <div className="p-2 bg-white rounded-full mr-4">
           <Image src={googleIcon} width={24} height={24} alt="google-icon" priority />
